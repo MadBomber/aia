27097f6e1a9e3ea986ccf7a7e0f5a54d06121f8a
Dewayne VanHoozer <dvanhoozer@gmail.com>
2023-12-01 17:35:21 -0600

    create source-able files for prompt ID completion and general file completion for bash, zsh and fish shells.

---
+++ lib/aia/aia_completion.fish
@@ -0,0 +1,34 @@
+# lib/aia/aia_completion.fish
+# Setup a prompt completion for use with the fish shell
+#
+# This script assumes that the system environment
+# variable PROMPTS_DIR has been set correctly
+
+function __fish_aia_complete
+  # Get the command line and current token
+  set -l cmd_line (commandline -opc)
+  set -l current_token (commandline -ct)
+  
+  # Check if we are currently completing the option that requires prompt IDs
+  if set -q cmd_line[2]
+    # Change directory to the prompts directory
+    if test -d $PROMPTS_DIR
+      pushd $PROMPTS_DIR
+      # Generate completions based on .txt files in the PROMPTS_DIR directory
+      for file in (find . -name "*.txt" -type f)
+        set file (string replace -r '\.txt$' '' -- $file)
+        set file (string replace -r '^\./' '' -- $file)
+        printf "%s\n" $file
+      end
+      popd
+    end
+  else
+    # Use the default file completion if we are not completing a prompt ID
+    complete -f -c aia -a "(commandline -ct)"
+  end
+end
+
+# Register the completion function for the aia command
+complete -c aia -a '(__fish_aia_complete)' -f
+
+
