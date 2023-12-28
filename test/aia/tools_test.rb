b95a60be98a904a08e86ec4d425d24c38fc0b1fd
Dewayne VanHoozer <dvanhoozer@gmail.com>
2023-12-28 14:11:04 -0600

    deleted all of the AIA::Tools related class unit tests.  The new architecture requires a new test architecture which is TBD

--- test/aia/tools_test.rb
+++ test/aia/tools_test.rb
@@ -4,66 +4,52 @@ require_relative '../test_helper'
 require_relative '../../lib/aia/tools'
 
 class ToolsTest < Minitest::Test
-  def test_initialize
-    tool = AIA::Tools.new
 
-    assert_equal :role,                 tool.role
-    assert_equal "tools",               tool.name
-    assert_equal "description",         tool.description
-    assert_equal "URL",                 tool.url
-    assert_equal "brew install tools",  tool.install
-  end
-
-
-  def test_installed_query
-    tool = AIA::Tools.new
-    tool.stub :name, "echo" do
-      assert tool.installed?
+  def setup
+    @klass = Class.new(Tools) do
+      meta name: 'TestTool', version: '1.0'
     end
   end
 
 
-  def test_help
-    tool      = AIA::Tools.new
-    tool.name = "ruby"
-    help_text = tool.help.downcase
-
-    assert help_text.include?('usage')
-    assert help_text.include?('ruby')
+  def test_inherited_registers_subclass
+    assert_includes(Tools.catalog, @klass._metadata)
   end
 
 
-  def test_version
-    tool          = AIA::Tools.new
-    tool.name     = 'ruby'
-    version_text  = tool.version.downcase
-    ruby_version  = RUBY_VERSION
-
-    assert version_text.start_with?("ruby #{RUBY_VERSION}")
+  def test_meta_assigns_metadata
+    assert_equal('TestTool', @klass.meta[:name])
+    assert_equal('1.0', @klass.meta[:version])
   end
 
 
-  def test_verify_tools
-    AIA::Tools.stub :verify_tools, nil do
-      assert_nil AIA::Tools.verify_tools
-    end
+  def test_get_meta_returns_metadata
+    assert_equal(@klass._metadata, @klass.get_meta)
   end
 
 
-  def test_format_missing_tools_response
-    fake_tool = Minitest::Mock.new
-    fake_tool.expect :name, 'fake'
-    fake_tool.expect :url, 'https://example.com/fake'
+  def test_search_for_finds_correct_subclasses
+    results = Tools.search_for(name: 'TestTool')
+    assert_equal(1, results.size)
+    assert_equal(@klass._metadata, results.first)
+  end
 
-    expected_response = <<EOS
 
-WARNING: AIA makes use of external CLI tools that are missing.
+  def test_catalog_returns_all_subclasses
+    new_klass = Class.new(Tools) { meta name: 'NewTool', version: '1.1' }
 
-Please install the following tools:
+    assert_includes(Tools.catalog.map { |meta| meta[:name] }, 'TestTool')
+    assert_includes(Tools.catalog.map { |meta| meta[:name] }, 'NewTool')
+  end
 
-  fake: install from https://example.com/fake
-EOS
 
-    assert_equal expected_response, AIA::Tools.format_missing_tools_response([fake_tool])
+  def test_search_for_with_no_criteria_returns_all
+    assert_equal(Tools.catalog, Tools.search_for({}))
+  end
+
+
+  def test_load_tools_require_files
+    Tools.load_tools
+    assert(Tools.catalog.any?, 'Expected Tools.catalog to have loaded subclasses')
   end
 end
